ad5980fdad9f473c8edadbabcc9491494861163c
Shreyasunkari249 <142834261+Shreyasunkari249@users.noreply.github.com>
2023-12-18 13:37:27 +0530

    Add files via upload

---
+++ 10p.js
@@ -0,0 +1,144 @@
+//1st program eligible to vote
+                            //    let num;
+                            //    function vote(num){
+                            //          if(num>=18){
+                            //            return "Eligible";  
+                            //      }
+                            //     else{
+                            //           return "NotEligible";
+                            //       }
+                            //     }
+                            //    console.log(vote(20));
+
+//2nd program 5-digit or not
+                                //    let num;
+                                //    function digit(num){
+                                //         if(num>=10000&&num<9999){
+                                //              return "5 digit number"
+                                //      }
+                                //       else{
+                                //              return "Not a 5digit number";
+                                //      }
+
+                                //       }
+                                //       console.log(digit(874398752))
+      
+//3rd program leap year or not
+                                    //    let year;
+                                    //    function leapOrNot(year){
+                                    //        if((year%400==0)&&(year%100==0)){
+                                    //                  return "leap year";
+                                    //         }
+                                    //         else if((year%4==0)&&(year%100!=0)){
+                                    //                 return "leap year"
+                                    //           }
+                                    //         else{
+                                    //                  return "not a leap year"
+                                    //          }
+                                    //     }     
+                                    //     console.log(leapOrNot(2023))            
+                        
+//4th program grade of student
+
+                                        //  let a;
+                                        // function demo(a){
+                                        //         if(a>80&&a<100){
+                                        //                    return"A";
+                                        //   }
+                                        // else if(a>70&&a<80){
+                                        //           return "B";
+                                        //  }
+                                        //    else if(a>70&&a<60){
+                                        //       return "c";
+                                        //      }
+                                        //   else if(a>60&&a<50){
+                                        //          return "d";
+                                        //  }
+                                        // }
+                                        //    console.log(demo(90));
+
+//5th program Vowel or not 
+                                            // let s;
+                                            // function isVowelOrNot(s){
+                                            //   value=s.toLowerCase();
+                                            //          if(value=='a'||value=='e'||value=='i'||value=='o'||value=='u'){
+                                            //             return "Vowel";
+                                            //     }
+                                            //             else{
+                                            //                return "Consonants";
+                                            //     }
+                                            //      } 
+                                            //   console.log(isVowelOrNot('a'))      
+
+//6th program max value
+                                            //   let a;
+                                            //   let max=0;
+                                            //    function myArray(a){
+                                            //         for(let i=0;i<a.length;i++){
+                                            //               if(max<a[i]){
+                                            //         max=a[i];
+                                            //          }
+                                            //         }
+                                            //        return max;
+                                            //        }                                            
+                                            //   console.log(myArray([23,12,34,21,46,76,23,78]));
+
+//7 th program min value
+                                                // let a;
+                                                // function myArray(a){
+                                                //        let min=a[0];
+                                                //          for(let i=1;i<a.length;i++){  
+                                                //             if(min>a[i]){
+                                                //                   min=a[i];
+                                                //              }
+                                                //        return min;
+                                                //        }
+                                                //     }
+                                                //    console.log(myArray([43,12,76,44,76,74,23]));
+
+//8th program 2nd max value
+                                                //    function secondLarge(arr) {
+                                                //       let max1=arr[0];
+                                                //      let max2=arr[0];
+                                                //  for(let i=1;i<arr.length;i++){
+                                                //    if(arr[i]>max1){
+                                                //           max2=max1;
+                                                //            max1=arr[i];
+                                                //    }
+                                                //  else if(arr[i]>max2 && arr[i]!=max1){
+                                                //           max2=arr[i];
+                                                //    }
+                                                //    }
+                                                //  return max2;
+                                                //    }
+                                                //  console.log(secondLarge([4,7,12,20]));
+
+//9th program 2nd min value
+                                        //   function secondSmall(array){
+                                        //        let small1=array[0];
+                                        //              let small2=Infinity;
+                                        //      for(let i=0;i<array.length;i++) {
+                                        //         if(array[i]<small1)  {
+                                        //            small2=small1;
+                                        //            small1=array[i];
+                                        //                 }
+                                        //      else if(array[i]>small1 && array[i]<small2) {
+                                        //              small2=array[2];
+                                        //                 }
+                                        //          }
+                                        //          return small2;
+                                        //              }
+                                        //     console.log(secondSmall([2,1,3,6,7,9]));                                 
+                                        
+//10th program sum of array
+                                              // let a;
+                                              // function myArray(a){
+                                              //     let sum=0;
+                                              //       for(let i=0;i<a.length;i++){
+                                              //             sum+=a[i];
+                                              //        }  
+                                              //     return sum;          
+                                              //         }                 
+                                              //   console.log(myArray([89,23,54,12,65,34,87,76,12,86,21,78]));                       
+
+ 
